a9183206db8aa143f862e6846b5600253119f02e
alantientran <att0316698@gmail.com>
2024-03-25 19:28:37 -0500

    dependencies

--- on-the-watch/src/App.js
+++ on-the-watch/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './Screens/Components/Navbar';
-import "bootstrap/dist/css/bootstrap.min.css"; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
-
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Screens/Components/Navbar";
+// import "bootstrap/dist/css/bootstrap.min.css"; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
 function App() {
-
-// database.ref().update(sampleData)
-//   .then(() => console.log('Sample data sent successfully!'))
-//   .catch(error => console.error('Error sending sample data:', error));
+  // database.ref().update(sampleData)
+  //   .then(() => console.log('Sample data sent successfully!'))
+  //   .catch(error => console.error('Error sending sample data:', error));
   return (
     <div className="App">
       <header className="App-header">
@@ -16,14 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
       </header>
     </div>
   );
