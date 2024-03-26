5982be0e3a6c735418c9628378b4e850d4323094
meghana2367 <maggy.chinta@gmail.com>
2024-03-25 19:21:12 -0500

    fixing things

--- on-the-watch/src/App.js
+++ on-the-watch/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Screens/Components/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css"; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+
 
 function App() {
 
---
+++ on-the-watch/src/Screens/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+    <div className="row" >
+        <p>Welcome Back _____</p>
+    </div>
+    </>
+  );
+};
+
+export default NavBar;
