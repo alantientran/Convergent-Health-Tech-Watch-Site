3c867aac8683c0f0b2cdfad28ddb59e072b9f401
SamuelJacob146 <samuel.t.jacob888@gmail.com>
2024-04-01 21:00:49 -0500

    until light and dark mode

--- on-the-watch/src/App.js
+++ on-the-watch/src/App.js
@@ -1,22 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
-import NavBar from "./Screens/Components/Navbar";
-// import "bootstrap/dist/css/bootstrap.min.css"; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+import {ColorModeContext, useMode} from './theme';
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
-  // database.ref().update(sampleData)
-  //   .then(() => console.log('Sample data sent successfully!'))
-  //   .catch(error => console.error('Error sending sample data:', error));
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <NavBar />
-      </header>
-    </div>
+    <ColorModeContext.Provider value = {colorMode}>
+        <ThemeProvider theme = {theme}>
+          <CssBaseline />
+          <div className="app">
+              <main className = "app">
+                <Topbar />
+              </main>
+          </div>
+        </ThemeProvider>
+    </ColorModeContext.Provider>
+
   );
 }
 
